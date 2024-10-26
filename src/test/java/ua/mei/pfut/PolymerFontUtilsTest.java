97fb52b22374fee6499521c871cab6015f58df55
MeiNanziiii <meinanziiii.ua@gmail.com>
2024-10-26 09:27:42 +0300

    remove test space

--- src/test/java/ua/mei/pfut/PolymerFontUtilsTest.java
+++ src/test/java/ua/mei/pfut/PolymerFontUtilsTest.java
@@ -2,14 +2,12 @@ package ua.mei.pfut;
 
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.text.Text;
 import ua.mei.pfu.font.FontResourceManager;
 import ua.mei.pfu.font.provider.BitmapFontProvider;
 
 public class PolymerFontUtilsTest implements ModInitializer {
     public static FontResourceManager manager;
     public static BitmapFontProvider bitmap;
-    public static Text space;
 
     @Override
     public void onInitialize() {
@@ -17,7 +15,5 @@ public class PolymerFontUtilsTest implements ModInitializer {
 
         manager = FontResourceManager.create("pfut", "test");
         bitmap = manager.requestBitmap("font/icon.png", 128, 64);
-        // Spaces not displaying in /pfu pfut:test
-        space = manager.requestSpace(356);
     }
 }
