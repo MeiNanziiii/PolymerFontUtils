a8d7d66dc6b8315fc12a83ceed72d8c905253204
MeiNanziiii <meinanziiii.ua@gmail.com>
2024-12-07 15:01:48 +0200

    bro 💀

--- src/main/java/ua/mei/pfu/api/provider/SpaceFontProvider.java
+++ src/main/java/ua/mei/pfu/api/provider/SpaceFontProvider.java
@@ -1,11 +1,12 @@
 package ua.mei.pfu.api.provider;
 
+import com.google.gson.annotations.Expose;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 
 import java.util.Map;
 
-public record SpaceFontProvider(String type, Map<String, Integer> advances) implements BaseFontProvider {
+public record SpaceFontProvider(@Expose String type, @Expose Map<String, Integer> advances) implements BaseFontProvider {
     public SpaceFontProvider(Map<String, Integer> advances) {
         this("space", advances);
     }
